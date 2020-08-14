c19edd2b7c8fecb02f3ff58b51d4ff47a65e0c5a
Crowdbotics <team@crowdbotics.com>
2020-08-14 20:36:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cooking_crazy_19565/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cooking_crazy_19565;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "cooking_crazy_19565";
+    }
+}
